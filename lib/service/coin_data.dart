fddd87ba566d2dc8849cad0d1dcc3e4fb3a6ba06
Yuta iwasaki <yanyong0127@gmail.com>
2022-03-21 16:25:05 +0900

    複数のやつやりたいけどむっず

--- lib/service/coin_data.dart
+++ lib/service/coin_data.dart
@@ -1,5 +1,6 @@
 import '../secret_consts.dart';
-import 'networkhelper.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 const List<String> currenciesList = [
   'AUD',
@@ -31,23 +32,43 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-const coinapiURLString = 'https://rest.coinapi.io/v1/exchangerate/';
+const coinapiURL = 'https://rest.coinapi.io/v1/exchangerate/';
 
 class CoinData {
   //List<decodeData>を取ってくる
-  Future<List<String>> fetchDataList(String currency) async {
+  Future fetchDataList(String currency) async {
     final List<Future<String>> futures = <Future<String>>[];
+
     Future<String> f(String coinapiURL) async {
-      var coinData = await NetworkHelper(coinapiURL).fetchData();
-      print(coinData);
-      return coinData.toStringAsFixed(0).padLeft(6, ' ');
+       print(coinapiURL + ' start');
+      http.Response response = await http.get(Uri.parse(coinapiURL));
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        //Create a new key value pair, with the key being the crypto symbol and the value being the lastPrice of that crypto currency.
+        print('lastPrice');
+        print(lastPrice);
+        print('lastPrice');
+        return lastPrice.toStringAsFixed(0).padLeft(6, ' ');
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
     }
-
+   
     cryptoList.forEach((crypto) {
-      futures.add(f('$coinapiURLString$crypto/$currency?apikey=$apikey'));
+      futures.add(f('$coinapiURL$crypto/$currency?apikey=$apikey'));
     });
+     print('Future.wait() start');
+     var futureWait = Future.wait(futures);
 
-    return await Future.wait(futures);
+print('start await');
+  var result = await futureWait;
+  print('result');
+  print(result);
+  print('result');
+
+    return  result;
 
     // return await Future.wait(cryptoList.map((crypto) {
     //   return f('$coinapiURLString$crypto/$currency?apikey=$apikey');
